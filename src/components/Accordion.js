e271fd6360ba64e8e86b7fcd86f28038360ab13d
Md Irfan <irfan02md@gmail.com>
2024-06-08 15:08:04 +0530

    fixing one accordion open issue

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 function Accordion({ data }) {
+
+    const [curOpen, setCurOpen] = useState(null);
+
     return (
         <div className="accordion">
-            {data.map((el, i) => <AccordionItem num={i} title={el.title} text={el.text} key={i+1}/>)}
+            {data.map((el, i) => <AccordionItem curOpen={curOpen} onOpen={setCurOpen} num={i} title={el.title} key={i+1}>{el.text}</AccordionItem>)}
         </div>
     );
 }
--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
 
-function AccordionItem({ num, title, text }) {
-    const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+    
+    const isOpen = num === curOpen;
 
     function handelToggle() {
-        setIsOpen(!isOpen);
+        onOpen(isOpen ? null : num);
     }
 
     return (
@@ -12,7 +12,7 @@ function AccordionItem({ num, title, text }) {
             <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
             <p className="title">{title}</p>
             <p className="icon">{isOpen ? "-" : "+"}</p>
-            {isOpen && <div className="content-box">{text}</div>}
+            {isOpen && <div className="content-box">{children}</div>}
         </div>
     );
 }
